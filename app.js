266f6a221e883db828899d5e27f7842666cabc53
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-28 15:55:11 +0200

    try to add bg img

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ createApp({
       explanation: "This is my first implementation of vue.js, I’m having fun replicating the website of Evan You, the creator of the mentioned framework…a genius!!!",
       details:"Vue.js is an open-source model-view-viewmodel front end JavaScript framework for building user interfaces and single-page applications",
       linkVue:"https://vuejs.org/",
-      linkButton:"Visit Vue.js.org"
+      linkButton:"Visit Vue.js.org",
+      image:{backgroundImage: "url(./bg-img/wp5502431-gradient-4k-wallpapers.jpg)"}
     };
   },
 }).mount("#app");
