ac66de03bce53cc040eb9e5bc8c4ec20a35b3648
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-28 15:31:42 +0200

    Add some details to the page with same method

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ createApp({
   data() {
     return {
       message: "Hello.",
+      presentation: "My name is Luca Ranieri",
+      explanation: "This is my first implementation of vue.js, I’m having fun replicating the website of Evan You, the creator of the mentioned framework…a genius!!!",
+      details:"Vue.js is an open-source model-view-viewmodel front end JavaScript framework for building user interfaces and single-page applications",
+      linkVue:"https://vuejs.org/",
+      linkButton:"Visit Vue.js.org"
     };
   },
 }).mount("#app");
